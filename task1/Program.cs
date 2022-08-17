6a64dda7454da89077ad518b43f0c251eefaa1b4
Nick <mikabrmail@gmail.com>
2022-08-18 02:15:14 +0300

    task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for(int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[] collect)
+{
+    int count = collect.Length;
+    for(int position = 0; position < count; position++)
+    {
+        Console.Write(collect[position] + " ");
+    }
+}
+
+int [] array = new int [8];
+
+FillArray(array);
+PrintArray(array);
