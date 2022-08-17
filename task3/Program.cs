33ccc286ace835d2d06fecaece9a03991fdda469
Nick <mikabrmail@gmail.com>
2022-08-18 02:15:37 +0300

    task3 complete

---
+++ task3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Написать программу замену элементов массива на противоположные
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for(int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(0,10);
+    }
+}
+
+void PrintArray(int[] collect)
+{
+    int count = collect.Length;
+    for(int index = 0; index < count; index++)
+    {
+        Console.Write(collect[index] + " ");
+    }
+}
+
+void ReverseArray(int[] collection)
+{
+    int size = collection.Length;
+    for(int index = 0; index < size/2; index++)
+    {
+        int temp = collection[index];
+        collection[index] = collection[size-index-1];
+        collection[size-index-1] = temp;
+    }
+}
+
+int [] array = new int [15];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ReverseArray(array);
+PrintArray(array);
